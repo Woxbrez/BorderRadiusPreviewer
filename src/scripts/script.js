12cee190502426e325722a7f045a5549b826072a
Daniel Soares <danielsoaresf@hotmail.com>
2020-10-23 20:04:34 -0300

    Four Border

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -12,3 +12,43 @@ function myFunction() {
     /* Alert the copied text */
     alert("Copied the text: " + copyText.value);
   }
+
+var boxIn = document.getElementById("box");
+var code = document .getElementsByClassName("code");
+code[0].value = "50% 50% 50% 50% / ";
+boxIn.style.borderTopLeftRadius = "50%";
+boxIn.style.borderTopRightRadius = "50%";
+boxIn.style.borderBottomRightRadius = "50%";
+boxIn.style.borderBottomLeftRadius = "50%";
+
+function radiusPreviewer(x, input){
+  let value = input.value;
+
+  switch(x){
+    case 'a':
+      console.log(boxIn.style.borderTopLeftRadius = value + "%");
+      break;
+    case 'b':
+      console.log(boxIn.style.borderTopRightRadius = value + "%");
+      break;
+    case 'c':
+      console.log(boxIn.style.borderBottomRightRadius = value + "%");
+      break;
+    case 'd':
+      console.log(boxIn.style.borderBottomLeftRadius = value + "%");
+      break;
+    case 'e':
+      console.log("e");
+      break;
+    case 'f':
+      console.log("f");
+      break;
+    case 'g':
+      console.log("g");
+      break;
+  }
+
+  code[0].value = boxIn.style.borderTopLeftRadius + " " + boxIn.style.borderTopRightRadius + " " + boxIn.style.borderBottomRightRadius + " " + boxIn.style.borderBottomLeftRadius + " / ";
+}
+
+//border-radius: 50% 50% 50% 50% / 50% 50% 50% 50%;
