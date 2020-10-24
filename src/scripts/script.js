e21cad7f9752f74e12bccc95ffd250d471595db5
Daniel Soares <danielsoaresf@hotmail.com>
2020-10-24 19:17:06 -0300

    8 Elements Border

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -15,40 +15,48 @@ function myFunction() {
 
 var boxIn = document.getElementById("box");
 var code = document .getElementsByClassName("code");
-code[0].value = "50% 50% 50% 50% / ";
-boxIn.style.borderTopLeftRadius = "50%";
-boxIn.style.borderTopRightRadius = "50%";
-boxIn.style.borderBottomRightRadius = "50%";
-boxIn.style.borderBottomLeftRadius = "50%";
+boxIn.style.borderRadius = "50% 50% 50% 50% / 50% 50% 50% 50%";
+code[0].value = boxIn.style.borderRadius;
+var border = ["50", "50", "50", "50", "50", "50", "50", "50"];
 
 function radiusPreviewer(x, input){
   let value = input.value;
 
   switch(x){
     case 'a':
-      console.log(boxIn.style.borderTopLeftRadius = value + "%");
+      boxIn.style.borderTopLeftRadius = value + "% " + border[4] + "%";
+      border[0] = value;
       break;
     case 'b':
-      console.log(boxIn.style.borderTopRightRadius = value + "%");
+      boxIn.style.borderTopRightRadius = value + "% " + border[5] + "%";
+      border[1] = value;
       break;
     case 'c':
-      console.log(boxIn.style.borderBottomRightRadius = value + "%");
+      boxIn.style.borderBottomRightRadius = value + "% " + border[6] + "%";
+      border[2] = value;
       break;
     case 'd':
-      console.log(boxIn.style.borderBottomLeftRadius = value + "%");
+      boxIn.style.borderBottomLeftRadius = value + "% " + border[7] + "%";
+      border[3] = value;
       break;
     case 'e':
-      console.log("e");
+      boxIn.style.borderTopLeftRadius = border[0] + "% " + value + "%";
+      border[4] = value;
       break;
     case 'f':
-      console.log("f");
+      boxIn.style.borderTopRightRadius = border[1] + "% " + value + "%";
+      border[5] = value;
       break;
     case 'g':
-      console.log("g");
+      boxIn.style.borderBottomRightRadius = border[2] + "% " + value + "%";
+      border[6] = value;
+      break;
+    case 'h':
+      boxIn.style.borderBottomLeftRadius = border[3] + "% " + value + "%";
+      border[7] = value;
       break;
   }
 
-  code[0].value = boxIn.style.borderTopLeftRadius + " " + boxIn.style.borderTopRightRadius + " " + boxIn.style.borderBottomRightRadius + " " + boxIn.style.borderBottomLeftRadius + " / ";
+  code[0].value = boxIn.style.borderRadius;
 }
 
-//border-radius: 50% 50% 50% 50% / 50% 50% 50% 50%;
